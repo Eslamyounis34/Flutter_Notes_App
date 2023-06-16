be805e5b2cb95aa9c718dbf2f9d76da58a220a3a
Eslam-Youness <eslam-youness@elarabygroup.com>
2023-06-16 18:23:18 +0300

    init hive in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hive_notes_app/shared/constants.dart';
 import 'package:hive_notes_app/shared/styles.dart';
 import 'package:hive_notes_app/views/notes_view.dart';
 
-void main() {
-  runApp(NotesApp());
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
+  runApp(const NotesApp());
 }
 
 class NotesApp extends StatelessWidget {

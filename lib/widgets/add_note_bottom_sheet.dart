a5f2c8f3aff66c0b1d20fee9b6c5bd9c33e5b5b1
Eslam-Youness <eslam-youness@elarabygroup.com>
2023-06-19 14:34:02 +0300

    listen to cubit inside add Note bottom sheet

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:hive_notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:hive_notes_app/shared/constants.dart';
 import 'package:hive_notes_app/widgets/custom_text_field.dart';
-
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'add_note_form.dart';
 import 'custom_button.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
-  const AddNoteBottomSheet({super.key});
+  AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,69 +19,23 @@ class AddNoteBottomSheet extends StatelessWidget {
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 12),
         child: SingleChildScrollView(
-          child: AddNoteForm(),
+          child: BlocConsumer<AddNoteCubit, AddNoteCubitState>(
+            listener: (context, state) {
+              if (state is AddNoteFailure) {
+                print('Failed ${state.errorMsg}');
+              }
+              if (state is AddNoteSuccess) {
+                Navigator.pop(context);
+              }
+            },
+            builder: (context, state) {
+              return ModalProgressHUD(
+                  inAsyncCall: state is AddNoteLoading ? true : false,
+                  child: AddNoteForm());
+            },
+          ),
         ),
       ),
-    );
-  }
-}
-
-class AddNoteForm extends StatefulWidget {
-  const AddNoteForm({
-    super.key,
-  });
-
-  @override
-  State<AddNoteForm> createState() => _AddNoteFormState();
-}
-
-class _AddNoteFormState extends State<AddNoteForm> {
-  final GlobalKey<FormState> formKey = GlobalKey();
-  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
-
-  String? title, content;
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      autovalidateMode: autovalidateMode,
-      child: Column(children: [
-        SizedBox(
-          height: 25,
-        ),
-        CustomTextField(
-          onSaved: (value) {
-            title = value;
-          },
-          hint: 'Title',
-          maxLines: 1,
-        ),
-        const SizedBox(
-          height: 16,
-        ),
-        CustomTextField(
-            onSaved: (value) {
-              content = value;
-            },
-            hint: 'Content',
-            maxLines: 5),
-        const SizedBox(
-          height: 32,
-        ),
-        CustomButton(
-          onTap: () {
-            if (formKey.currentState!.validate()) {
-              formKey.currentState!.save();
-            } else {
-              autovalidateMode = AutovalidateMode.always;
-              setState(() {});
-            }
-          },
-        ),
-        const SizedBox(
-          height: 22,
-        )
-      ]),
     );
   }
 }

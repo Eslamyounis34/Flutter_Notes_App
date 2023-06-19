e74efe742990b44af3bd27ca16210e57373be2b3
Eslam-Youness <eslam-youness@elarabygroup.com>
2023-06-19 04:24:15 +0300

    register Adapter

--- lib/cubits/add_note_cubit/add_note_cubit_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit_cubit.dart
@@ -1,8 +1,11 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_notes_app/models/note_model.dart';
 import 'package:meta/meta.dart';
 
 part 'add_note_cubit_state.dart';
 
 class AddNoteCubitCubit extends Cubit<AddNoteCubitState> {
   AddNoteCubitCubit() : super(AddNoteCubitInitial());
+
+  addNote(NoteModel note) {}
 }

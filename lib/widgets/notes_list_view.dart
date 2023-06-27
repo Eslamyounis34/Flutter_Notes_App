3bbe1a740f8accaf35bd27dfadb1a481ca61497d
Eslam-Youness <eslam-youness@elarabygroup.com>
2023-06-27 17:49:11 +0300

    adding build condition for empty list

--- lib/widgets/notes_list_view.dart
+++ lib/widgets/notes_list_view.dart
@@ -1,4 +1,6 @@
+import 'package:buildcondition/buildcondition.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:hive_notes_app/widgets/note_item.dart';
@@ -15,19 +17,27 @@ class NotesListView extends StatelessWidget {
       builder: (context, state) {
         List<NoteModel>? notes = BlocProvider.of<NotesCubit>(context).notesList;
         print('testScreenList : ' + notes.toString());
-        return Padding(
-          padding: const EdgeInsets.only(top: 18.0),
-          child: ListView.builder(
-              itemCount: notes?.length,
-              padding: EdgeInsets.zero,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: EdgeInsets.symmetric(vertical: 4),
-                  child: NoteItem(
-                    note: notes![index],
-                  ),
-                );
-              }),
+        return BuildCondition(
+          condition: notes!.isNotEmpty,
+          builder: (context) => Padding(
+            padding: const EdgeInsets.only(top: 18.0),
+            child: ListView.builder(
+                itemCount: notes?.length,
+                padding: EdgeInsets.zero,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.symmetric(vertical: 4),
+                    child: NoteItem(
+                      note: notes![index],
+                    ),
+                  );
+                }),
+          ),
+          fallback: (context) => const Center(
+              child: Text(
+            'Add Your First Note',
+            style: TextStyle(color: Colors.white, fontSize: 22),
+          )),
         );
       },
     );
